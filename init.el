a5463e2b74e26a32c55b0e5f74d73dbe8db0d1a6
uryu1994 <prices_over@uryu1994.work>
2017-08-06 11:57:51 +0900

    Initialize Commit

---
+++ init.el
@@ -0,0 +1,57 @@
+;; package
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+
+;; 行番号設定 
+(require 'linum)
+(global-linum-mode)
+
+;; バックアップファイル作成禁止
+(setq make-backup-files nil)
+
+;; 自動セーブ禁止
+(setq auto-save-default nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (omnisharp company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; company.el
+(require 'company)
+(global-company-mode) ; 全バッファで有効にする 
+(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-minimum-prefix-length 2) ; デフォルトは4
+(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+
+;; OmniSharp
+(setq omnisharp-server-executable-path "/usr/local/bin/omnisharp")
+(add-hook 'csharp-mode-hook 'omnisharp-mode)
+(eval-after-load
+ 'company
+ '(add-to-list 'company-backends 'company-omnisharp))
+
+;; 括弧を光らせる
+(show-paren-mode)
+
+;; Gauche
+(setq scheme-program-name "gosh")
+(require 'cmuscheme)
+(defun scheme-other-window ()
+  "Run scheme on other window"
+  (interactive)
+  (switch-to-buffer-other-window
+   (get-buffer-create "*scheme*"))
+  (run-scheme scheme-program-name))
+(define-key global-map "\C-cS" 'scheme-other-window)
+
